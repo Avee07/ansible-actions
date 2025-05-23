657dc6ebe740a23f43dc6f8216efeca4b75480a0
Arwinder Singh <arwinder7024@gmail.com>
2025-05-23 16:55:18 +0530

    testing s3

---
+++ test_s3.py
@@ -0,0 +1,26 @@
+import boto3
+from botocore.client import Config
+import os
+
+endpoint = os.environ["S3_ENDPOINT"]
+access_key = os.environ["S3_ACCESS_KEY"]
+secret_key = os.environ["S3_SECRET_KEY"]
+bucket_name = os.environ["S3_BUCKET"]
+
+# Connect to MinIO
+s3 = boto3.resource(
+    's3',
+    endpoint_url=endpoint,
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    config=Config(signature_version='s3v4'),
+    region_name='us-east-1'
+)
+
+# Upload a file (or create a sample one)
+with open("hello.txt", "w") as f:
+    f.write("Hello from GitHub Actions!")
+
+s3.Bucket(bucket_name).upload_file("hello.txt", "hello.txt")
+
+print(f"✅ Uploaded 'hello.txt' to bucket '{bucket_name}'")
